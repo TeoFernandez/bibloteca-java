62785e7efe0ae3068ff0edaa2af1dcc87b681558
Nicolas Garcia <nicolasagustingarcia46@gmail.com>
2025-06-29 11:59:48 -0300

    Agrega archivos y funcionalidad para gestionar libros: agregar, eliminar y modificar.

--- front-end/js/misComprobantes.js
+++ front-end/js/misComprobantes.js
@@ -5,32 +5,41 @@ export const MISCOMPROBANTES_SERVICE = "http://localhost:8080";
 
 addEventListener("DOMContentLoaded", ()=>{
 
-    opcBuscar.addEventListener("click", buscarComprobantes);
-    opcNuevo.addEventListener("click", nuevoComprobante);
+    opcagregarlibro.addEventListener("click", agregarlibro);
+    opceliminarlibro.addEventListener("click", eliminarlibro);
+    opcmodificarlibro.addEventListener("click", modificarlibro);
 
 });
 
-async function buscarComprobantes() {
+async function agregarlibro() {
 
-    principalHeader.innerText = "Buscar";
+    principalHeader.innerText = "Agregar libro";
 
-    await loadComponent("components/buscar/html/index.html", 
-                  "components/buscar/css/buscar.css",
-                  "components/buscar/js/buscar.js");
+    await loadComponent("components/libros/agregar_libro/html/index.html", 
+                  "components/libros/agregar_libro/css/agregarlibro.css",
+                  "components/libros/agregar_libro/js/agregarlibro.js");
 
 
     initBuscarComponente();
 }
 
-async function nuevoComprobante() {
+async function eliminarlibro() {
 
-    principalHeader.innerText = "Nuevo";
+    principalHeader.innerText = "Eliminar Libro";
 
-    await loadComponent("components/nuevo/html/index.html", 
-                  "components/nuevo/css/nuevo.css",
-                  "components/nuevo/js/nuevo.js");
+    await loadComponent("components/libros/eliminar_libro/html/index.html", 
+                  "components/libros/eliminar_libro/css/eliminar_libro.css",
+                  "components/libros/eliminar_libro/js/eliminar_libro.js");
 
     initNuevoComponente();
+}
+
+async function modificarlibro() {
+    principalHeader.innerText="Modificar Libro";
+
+    await loadComponent("components/libros/modificar_libro/html/index.html",
+                  "components/libros/eliminar_libro/css/modificar_libro.css",
+                  "components/libros/eliminar_libro/js/modificar_libro.js");
 }
 
 async function loadComponent(html, css, js) {
