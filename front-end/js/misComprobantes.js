96f286ede1b10aa5c7ba5322b4397e5c3241c740
Nicolas Garcia <nicolasagustingarcia46@gmail.com>
2025-06-29 18:57:01 -0300

    Agrega campos para ID en formularios de modificación de alumno y libro, y crea la interfaz de historial de préstamos y solicitud de préstamos.

--- front-end/js/misComprobantes.js
+++ front-end/js/misComprobantes.js
@@ -4,7 +4,6 @@ import { initBuscarComponente } from "../components/buscar/js/buscar.js";
 export const MISCOMPROBANTES_SERVICE = "http://localhost:8080";
 
 addEventListener("DOMContentLoaded", ()=>{
-
     opcagregarlibro.addEventListener("click", agregarlibro);
     opceliminarlibro.addEventListener("click", eliminarlibro);
     opcmodificarlibro.addEventListener("click", modificarlibro);
@@ -14,6 +13,9 @@ addEventListener("DOMContentLoaded", ()=>{
     opcagregarusuario.addEventListener("click", agregarusuario);
     opcmodificarusuario.addEventListener("click", modificarusuario);
     opceliminarusuario.addEventListener("click", eliminarusuario);
+    opcsolicitarprestamo.addEventListener("click", solicitarprestamo);
+    opcdevolucionprestamo.addEventListener("click", devolucionprestamo);
+    opchistorialprestamo.addEventListener("click", historialprestamo);
 });
 
 async function agregarlibro() {
@@ -43,8 +45,44 @@ async function modificarlibro() {
     principalHeader.innerText="Modificar Libro";
 
     await loadComponent("components/libros/modificar_libro/html/index.html",
-                  "components/libros/eliminar_libro/css/modificar_libro.css",
-                  "components/libros/eliminar_libro/js/modificar_libro.js");
+                  "components/libros/modificar_libro/css/modificar_libro.css",
+                  "components/libros/modificar_libro/js/modificar_libro.js");
+}
+
+async function solicitarprestamo() {
+
+    principalHeader.innerText = "Solicitar Prestamo";
+
+    await loadComponent("components/prestamos/solicitud/html/index.html", 
+                  "components/prestamos/solicitud/css/styles.css",
+                  "components/prestamos/solicitud/js/agregarlibro.js");
+
+
+    initBuscarComponente();
+}
+
+async function devolucionprestamo() {
+
+    principalHeader.innerText = "Devolucion Prestamo";
+
+    await loadComponent("components/prestamos/eliminar/html/index.html", 
+                  "components/prestamos/eliminar/css/styles.css",
+                  "components/prestamos/eliminar/js/agregarlibro.js");
+
+
+    initBuscarComponente();
+}
+
+async function historialprestamo() {
+
+    principalHeader.innerText = "Historial Prestamo";
+
+    await loadComponent("components/prestamos/historial/html/index.html", 
+                  "components/prestamos/historial/css/styles.css",
+                  "components/prestamos/historial/js/agregarlibro.js");
+
+
+    initBuscarComponente();
 }
 
 async function agregaralumno() {
